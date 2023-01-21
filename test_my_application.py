d9a7c95afdde12aa26d13ac4b16680390b340f2e
ThamodrenSinniah <75205588+ThamodrenSinniah@users.noreply.github.com>
2023-01-21 18:51:43 +0800

    Playwright1 (#3)
    * Added double-quite-string-fixer preccommit
    
    * Added test with VSMONITOR
    
    * Updated packages and .gitignore
    
    * Changed headless to true

--- test_my_application.py
+++ test_my_application.py
@@ -1,21 +1,38 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, Playwright, expect
 
 
 def test_playwright(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto('https://playwright.dev/python/')
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    expect(page).to_have_title(re.compile('Playwright'))
 
     # create a locator
-    get_started = page.get_by_role("link", name="Get started")
+    get_started = page.get_by_role('link', name='Get started')
 
     # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
+    expect(get_started).to_have_attribute('href', '/python/docs/intro')
 
     # Click the get started link.
     get_started.click()
 
     # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
+    expect(page).to_have_url(re.compile('.*intro'))
+
+
+def test_run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://vsmonitor.com/')
+    page.get_by_role('link', name='angle-right icon Sign up').click()
+    page.locator('#eula_button').click()
+    page.get_by_role('button', name='Accept').click()
+    page.locator('#tos_button').click()
+    page.get_by_role('button', name='Accept').click()
+    page.locator('#data-privacy_button').click()
+    page.get_by_role('button', name='Accept').click()
+    page.get_by_role('button').filter(has_text='close').click()
+    context.close()
+    browser.close()
